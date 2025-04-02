345b13340eb1f59f7061291a139a03188e26f798
otavioluism <otavioluism97@hotmail.com>
2025-04-02 08:30:21 -0300

    add method GET with queryParams(RequestParam) and pathParams(PathVariable)

--- api-spring-boot/src/main/java/br/com/otavioluism/controller/FirstController.java
+++ api-spring-boot/src/main/java/br/com/otavioluism/controller/FirstController.java
@@ -1,17 +1,28 @@
 package br.com.otavioluism.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/firstController")
 public class FirstController {
 
-    @GetMapping("/")
-    public String firstFunctionController() {
-        return "Hello bati aqui na api agora!";
+    @GetMapping("/primeiroMetodo/{ID}")
+    public String firstFunctionController(@PathVariable(name = "ID") String id) {
+        return "O parâmetro é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam(name = "teste") String id){
+        return "O paramétro com metodoComQueryParams é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams(@RequestParam Map<String, String> allParams){
+        System.out.println(allParams);
+        return "O paramétro com metodoComQueryParams2 é " + allParams.entrySet();
     }
 
 }
