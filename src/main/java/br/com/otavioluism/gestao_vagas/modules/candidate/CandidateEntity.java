9e0920fd97264f227868386b0ba46589f0d4f595
otavioluism <otavioluism97@hotmail.com>
2025-04-12 14:37:03 -0300

    adicionado configuração para integrar a entidade do spring com uma tabela no postgres

--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,28 @@
 package br.com.otavioluism.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @NotBlank()
@@ -25,6 +36,12 @@ public class CandidateEntity {
     private String password;
 
     private String description;
+
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
