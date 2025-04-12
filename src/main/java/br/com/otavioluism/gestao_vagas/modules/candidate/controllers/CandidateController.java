a47259c7eac70eb77393a681a98265c0ed6c8493
otavioluism <otavioluism97@hotmail.com>
2025-04-12 14:57:01 -0300

    criando camada do repository para se comunicar sobre as ações do JPA com o postregres

--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,16 +1,23 @@
 package br.com.otavioluism.gestao_vagas.modules.candidate.controllers;
 
 import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        CandidateEntity candidate = this.candidateRepository.save(candidateEntity);
         System.out.println(candidateEntity);
+        return candidate;
     }
 
 }
