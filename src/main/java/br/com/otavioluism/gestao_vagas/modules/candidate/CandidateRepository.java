9ead61d3408e69a0b9c8d39a5c7231ee2a6e6068
otavioluism <otavioluism97@hotmail.com>
2025-04-16 22:43:13 -0300

    segregando os pacotes, criando camando de service e adicionando consulta na base de dados utilizando JPA

--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -2,8 +2,10 @@ package br.com.otavioluism.gestao_vagas.modules.candidate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email); // JPA é inteligente para saber que o metodo é de busca so olhando pelo findById, lembrando que se usar And ele precisa dos dois campos
 }
--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,9 +1,12 @@
 package br.com.otavioluism.gestao_vagas.modules.candidate.controllers;
 
+import br.com.otavioluism.gestao_vagas.exceptions.UserFoundException;
 import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.otavioluism.gestao_vagas.modules.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,13 +14,17 @@ import org.springframework.web.bind.annotation.*;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        CandidateEntity candidate = this.candidateRepository.save(candidateEntity);
-        System.out.println(candidateEntity);
-        return candidate;
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var response =  this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(response);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+
     }
 
 }
