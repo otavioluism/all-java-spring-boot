3661d6bd9394be0b663d30bb0b363e259b11e27c
otavioluism <otavioluism97@hotmail.com>
2025-04-18 14:35:06 -0300

    Criando entidade de company e também do job e o relacionamento entre as duas entidades

---
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,45 @@
+package br.com.otavioluism.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
+    private String username;
+
+    @Email(message = "O campo (email) deve conter um e-mail válido")
+    @Column(name = "email")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+    private String password;
+
+    private String webSite;
+
+    private String name;
+
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,36 @@
+package br.com.otavioluism.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id", updatable = false, insertable = false)
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
