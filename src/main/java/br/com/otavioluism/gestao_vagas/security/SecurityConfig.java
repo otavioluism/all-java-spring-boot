e6a654baebc85fdb77d80140dd347367a17c9027
otavioluism <otavioluism97@hotmail.com>
2025-04-25 08:41:55 -0300

    config rota publica e rota autenticada com spring security

--- src/main/java/br/com/otavioluism/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package br.com.otavioluism.gestao_vagas.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +10,12 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll() // esta rota permitimos nao ter autenticacao (publica)
+                            .requestMatchers("/company/").permitAll(); // esta rota permitimos nao ter autenticacao (publica)
+                    auth.anyRequest().authenticated(); // qualquer outra rota deve ter autenticacao
+                });
         return http.build();
     }
 
